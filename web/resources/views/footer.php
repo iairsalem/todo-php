a9c37bab2d66bb2c23951d883041a182765f05ca
iairsalem <iair1234@gmail.com>
2020-07-27 04:34:06 -0400

    footer bug

--- web/resources/views/footer.php
+++ web/resources/views/footer.php
@@ -3,7 +3,7 @@
 ?>
 <footer class="page-footer font-small">
   <!-- Copyright -->
-  <div style="font-size:0.50rem;" class="footer-copyright text-center py-3 small">© 2020 (<span id="last_commit"><?php echo get_branch_head('dev');?></span>) Copyleft:
+  <div style="font-size:0.50rem;" class="footer-copyright text-center py-3 small">© 2020 (<span id="last_commit"></span>) Copyleft:
     <a href="https://github.com/iairsalem/todo-php/">me</a>
   </div>
   <!-- Copyright -->
